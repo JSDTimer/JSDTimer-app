4f33e3f1d3818638aa3c5872187fb82214eb1c32
Declan <decavacado@gmail.com>
2023-07-21 16:07:28 -0400

    Initialization: First Commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 40}}>Hello JSDTimer</Text>
+    </View>
+  );
+};
+
+export default App;
