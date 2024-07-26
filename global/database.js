435024260d252125d77eea197318099a1a79ff7f
Declan <decavacado@gmail.com>
2024-07-25 22:14:05 -0400

    Databse skeleton...

--- global/database.js
+++ global/database.js
@@ -1,1 +1,11 @@
 //Contains Database Creation and Modification Functions
+import { MMKVLoader } from "react-native-mmkv-storage";
+
+
+export function create(name) {
+    return new MMKVLoader().withInstanceID(name).initialize();
+}
+
+export function addListeners(MMKV) {
+    //Add Listeners to MMKV instance
+}
