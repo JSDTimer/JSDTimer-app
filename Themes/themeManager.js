008415fade8f5ec04af8caff9791248ee4fe8f0f
Declan <decavacado@gmail.com>
2025-02-12 13:14:04 -0500

    BlueThing theme added

--- Themes/themeManager.js
+++ Themes/themeManager.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import {default as defaultTheme } from './default.json';
+import React from "react";
+import {default as defaultTheme } from "./default.json";
+import {default as blueThing } from "./bluething.json";
 
 
 //All themes here
 export const themes = {
     defaultTheme,
+    blueThing
 };
 
 //Theme Context
